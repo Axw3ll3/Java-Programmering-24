51385681ba61ad5b33cb060ecd501754435e651b
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-08-30 14:15:35 +0200

    Create JavaPlayground.java
    Skapande av program och importering av skanner

---
+++ codealongs/HelloWorld/Personlig/JavaPlayground.java
@@ -0,0 +1,19 @@
+package codealongs.HelloWorld.Personlig;
+import java.util.Scanner; 
+
+public class JavaPlayground {
+
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner (System.in);
+    
+        System.out.println("Hej och välkommen till denna simulation");
+    System.out.println("Idag ska vi ta reda på om du är tillräckligt gammal för att kunna delta i denna övning");
+
+    int age = sc.nextInt();
+
+
+    }
+    
+}
