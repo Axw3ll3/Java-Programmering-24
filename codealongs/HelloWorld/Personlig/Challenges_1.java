e23fb24b635a6055d6d1fb1d89b9c307d1251f3e
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-09-15 01:51:27 +0200

    New change

--- codealongs/HelloWorld/Personlig/Challenges_1.java
+++ codealongs/HelloWorld/Personlig/Challenges_1.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import javax.print.attribute.standard.NumberOfDocuments;
 
 public class Challenges_1 {
     private Scanner sc = new Scanner (System.in);
@@ -10,7 +11,7 @@ public Scanner getScanner() {
     return sc;
     }
 
-
+/*
 public double getNumbers() {
     System.out.println("Please enter 3 numbers: ");
     System.out.println("Number 1: ");
@@ -19,8 +20,44 @@ public double getNumbers() {
     double secondNumber = sc.nextDouble();
     System.out.println("Number 3: ");
     double thirdNumber = sc.nextDouble();
+    return 
     
 }
-public 
+    */
+public int getANumber() {
+    Scanner sc = getScanner();
+    System.out.println("Please, enter a number: ");
+    int Number = sc.nextInt();
+    Number = checkNumber(sc);
+    Number = checkValue();
+    return Number;
+
+    
+}
+
+
+public int checkNumber(Scanner sc) {
+    while (!sc.hasNextInt()) {
+        sc.nextLine();
+    }
+        return sc.nextInt();
+    }
+
+    public int checkValue(int Number) {
+
+        if (Number > 0) {
+            System.out.println("The number is positive");
+        }
+
+        else if (Number < 0) {
+            System.out.println("The number is negative");
+        }
+
+        else {
+            System.out.println("Number is zero");
+        }
+        return Number;
+    }
 
 }
+
