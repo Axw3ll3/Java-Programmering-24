c88ade3f756fa6900a2171b8254558b86477c1f6
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-09-15 18:54:40 +0200

    Alot of changes

--- codealongs/HelloWorld/Personlig/Challenges_1.java
+++ codealongs/HelloWorld/Personlig/Challenges_1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Challenges_1 {
     private Scanner sc = new Scanner (System.in);
     public void main(String[] args) {
-          getNumbers();
+          diamondPyramid();
 
 
 
@@ -14,32 +14,35 @@ public Scanner getScanner() {
     return sc;
     }
 
-
-public double getNumbers() {
+/*
+public void getNumbers() {
     System.out.println("Please enter 3 numbers: ");
     System.out.println("Number 1: ");
-    double firstNumber = sc.nextDouble();
+    double firstNumber = checkNumber(sc);
     System.out.println("Number 2: ");
-    double secondNumber = sc.nextDouble();
+    double secondNumber = checkNumber(sc);
     System.out.println("Number 3: ");
-    double thirdNumber = sc.nextDouble();
-    return;
+    double thirdNumber = checkNumber(sc);
+
+    
     
 }
-    /*
+*/
+
+/*
 public int getANumber() {
     Scanner sc = getScanner();
     int Number = 0;
-    System.out.println("Please, enter a number: ");
+    System.out.println("Please, enter a number you want to see multiplication on: ");
     Number = checkNumber(sc);
-    Number = checkValue(Number);
+    Number = multiplicationConverter(Number);
     return Number;
 
     
 }
 */
 
-public double checkNumber(Scanner sc) {
+public int checkNumber(Scanner sc) {
     while (!sc.hasNextInt()) {
         System.out.println("Please write a number");
         sc.nextLine();
@@ -47,29 +50,118 @@ public double checkNumber(Scanner sc) {
         return sc.nextInt();
     }
 
-    public double checkValue(double firstNumber, double secondNumber, double thirdNumber) {
+    /*
+    public double checkValue(double Number) {
 
-        if (Number > 0) {
+        if (Number == 0) {
+            System.out.println("The number is zero");
+        }
+
+        else if (Number > 0) {
             System.out.println("The number is positive");
         }
 
         else if (Number < 0) {
-            System.out.println("The number is negative");
-        }
-
-        else if (Number < 1) {
-            System.out.println("The number is small");
+            System.out.println("The number is small and negative");
         }
 
         else if (Number > 1000000) {
-            System.out.println("The number is big");
+            System.out.println("The number is big and positive");
         }
 
-        else {
-            System.out.println("Number is zero");
-        }
+
         return Number;
+    }
+  */  
+
+  /*
+    public int convertNumberToDay(int Number) {
+
+        if (Number == 1) {
+            System.out.println("Monday");
+        }
+
+        else if (Number == 2) {
+            System.out.println("Tuesday");
+        }
+
+        else if (Number == 3) {
+            System.out.println("Wednesday");
+        }
+
+        else if (Number == 4) {
+            System.out.println("Thursday");
+        }
+
+        else if (Number == 5) {
+            System.out.println("Friday");
+        }
+
+        else if (Number == 6) {
+            System.out.println("Saturday");
+        }
+
+        else if (Number == 7) {
+            System.out.println("Sunday");
+        }
+
+        return Number;
+    }
+    */
+
+    /* 
+    public int multiplicationConverter(int Number) {
+        for (int i = 0; i<10; i++) {
+            System.out.println(Number + " x " + (i + 1) + " = " + (Number * (i + 1)));
+            
+        }
+    return Number;
+  }
+    */
+
+   
+    /*
+    public void halfPyramidOfNumbers() {
+        System.out.println("How many rows of the pyramid do you want?");
+        int nrOfRows = sc.nextInt();
+
+        int rowCount = 1;
+        System.out.println("Here's your pyramid");
+
+        for (int i = nrOfRows; i > 0; i--) {
+            for (int j = 1; j<=i; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int j = 1; j<=rowCount; j++) {
+                System.out.print(rowCount + " ");
+            }
+            System.out.println();
+
+            rowCount++;
+
+        }
+        
+    }
+        */
+
+    public void diamondPyramid() {
+        int nrOfRows = sc.nextInt();
+        int start = 1;
+        int n;
+
+        do { 
+          n = 1;
+          
+          do {
+            System.out.print(" ");
+          }while (++n <= nrOfRows - start +1);
+        
+        
+        do { 
+          System.out.println("*");  
+        } while (++n <= nrOfRows * 2 - 1);
+        System.out.print("");
     }
 
 }
-
