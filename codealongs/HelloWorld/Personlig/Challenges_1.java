92abee2a0358a83df5beba04dfc1c0c47dec60c6
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-09-15 21:30:38 +0200

    Jobb på pyramiden

--- codealongs/HelloWorld/Personlig/Challenges_1.java
+++ codealongs/HelloWorld/Personlig/Challenges_1.java
@@ -165,21 +165,19 @@ public int checkNumber(Scanner sc) {
 
             rowCount++;        }
 
-
-        for (int i = nrOfRows; i < 0; i++) {
-            for (int j = 1; j<=i; j++) {
+ 
+        for (int i = nrOfRows; i>=1; i--) {
+            for (int j = 1; j <= i-1; j++) {
                 System.out.print(" ");
             }
             
-            for (int j = 1; j<=rowCount; j--) {
+            for (int j = 1; j<=(i-1); j++) {
                 System.out.print("*" + " ");
             }
             System.out.println();
 
-            rowCount--;
-
         }
-        
+        rowCount--;
     }
         
     }
