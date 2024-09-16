03b6fb242688d0b322885ded4eea6cb45f7d3332
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-09-16 15:59:48 +0200

    Car done

--- codealongs/HelloWorld/Personlig/E3_Uppgift.java
+++ codealongs/HelloWorld/Personlig/E3_Uppgift.java
@@ -1,4 +1,5 @@
    
+        /*
         class Book {
             private String title;
             private String author;
@@ -16,51 +17,76 @@
             System.out.println("Year: " +year);
         }
     
-    
+    */
         
     
-        
-    public static void main(String[] args) {
-        Book book = new Book();
-        book.printDetails();
-        Student_Class Student = new Student_Class("Axel", 12, 3.4);
-        Student.printStudentDetails();
     
-        }
-    }
-
-
+    /*
+    
     class Student_Class{
         private String name;
         private int age;
         private double grade;
-    
+        
         public Student_Class(String name, int age, double grade) {
             this.name=name;
             this.age=age;
             this.grade=grade;
-        }
-        
-        public void printStudentDetails() {
-            System.out.println("Name: " +name);
-            System.out.println("Age: " + age);
-            System.out.println("Grade: "+ grade);
-        }
-    }
-        
+            }
+            
+            public void printStudentDetails() {
+                System.out.println("Name: " +name);
+                System.out.println("Age: " + age);
+                System.out.println("Grade: "+ grade);
+                }
+                }
+                
+                */
+                
+                class Car {
+                    private String make;
+                    private String model;
+                    private int year;
+                    private String color;
+                    
+                    public Car(String make, String model, int year, String color) {
+                        this.make=make;
+                        this.model=model;
+                        this.year=year;
+                        this.color=color;
+                        
+                    }
+                    
+                    public Car(String make, String model, int year) {
+                        this(make, model, year, "Unknown");
+                    }
 
 
-    class Car_Class {
-        private String make;
-        private String model;
-        private int year;
-        private String color;
+                    public Car(String make) {
+                        this(make, "Unknown", 0, "Unknown");
+                    }
 
-    public Car_Class(String make, String model, int year, String color) {
-        this.make=make;
-        this.model=model;
-        this.year=year;
-        this.color=color;
-
-    }
-    }
+                    
+                    public void carPrintDetails() {
+                        System.out.println("Make: " +make);
+                        System.out.println("Model: " +model);
+                        System.out.println("Year: " +year);
+                        System.out.println("Color: " +color);
+                    }
+                    
+                    
+                public static void main(String[] args) {
+    
+                
+                    Car car1 = new Car("Toyota", "Camry", 2022, "Blue");
+                    car1.carPrintDetails();
+                    
+                    Car car2 = new Car("Honda", "Accord", 2023);
+                    car2.carPrintDetails();
+                    
+                    Car car3 = new Car ("Ford");
+                    car3.carPrintDetails();
+                
+                    }
+                    
+}
