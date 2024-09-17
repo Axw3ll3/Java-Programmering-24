062b85967e58c4df313e954759a00fab4ede3168
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-09-17 12:56:15 +0200

    Start of challenge 3

--- codealongs/HelloWorld/Personlig/Challenges_3.java
+++ codealongs/HelloWorld/Personlig/Challenges_3.java
@@ -1,6 +1,29 @@
 public class Challenges_3 {
-    public static void main(String[] args) {
-        
-    }
     
+    class House_Class {
+        private double area;
+        private int floors;
+        private int buildYear;
+        private String adress;
+        
+        public House_Class(double area, int floors, int buildYear, String adress) {
+            this.area=area;
+            this.floors=floors;
+            this.buildYear=buildYear;
+            this.adress=adress;
+        }
+        
+        public void houseDetails() {
+            System.out.println("House area: " +area+ " cm^2");
+            System.out.println("Amount of floors: " +floors);
+            System.out.println("Year the house was built: "+buildYear);
+            System.out.println("House adress: " +adress);
+        }
+    
+    
+        public static void main(String[] args) {
+        House_Class House = new House_Class(74,6, 4, 1990, "Berggatan 12");
+        House.houseDetails();
+    }
+}
 }
