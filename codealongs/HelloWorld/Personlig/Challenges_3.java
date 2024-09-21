da4fb7d2e651726fa5cbd7cee1d9c4b072685ccb
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-09-21 10:21:29 +0200

    Challenge 3 continuation

--- codealongs/HelloWorld/Personlig/Challenges_3.java
+++ codealongs/HelloWorld/Personlig/Challenges_3.java
@@ -21,10 +21,29 @@
     
     
         public static void main(String[] args) {
-        House House = new House(74.6, 4, 1990, "Berggatan 12");
-        House.houseDetails();
-        Human Human = new Human("Axel", "Construction worker", 21, "Man", 170);
-        Human.printHumanDetails();
+        House House1 = new House(74.6, 4, 1990, "Berggatan 12");
+        House House2 = new House (56.2, 2, 2010, "Bullerbyvägen 56");
+        House House3 = new House (100.5, 6, 2015, "Klubbhusgatan 27");
+        
+        /*
+        House1.houseDetails();
+        System.out.println("");
+        House2.houseDetails();
+        System.out.println("");
+        House3.houseDetails();
+        System.out.println("");
+        */
+        Human Human1 = new Human("Axel", "Construction worker", 21, "Man", 170, House1);
+        Human1.printHumanDetails();
+        System.out.println("");
+
+        Human Human2 = new Human("Karl", "Programmer", 32, "Man", 186, House2);
+        Human2.printHumanDetails();
+        System.out.println("");
+
+        Human Human3 = new Human("Tilda", "Editor", 27, "Woman", 167, House3);
+        Human3.printHumanDetails();
+        System.out.println("");
     }
 }
 
@@ -35,13 +54,15 @@
             private int age;
             private String gender;
             private double length;
+            private House house;
 
-        public Human(String name, String work, int age, String gender, double length){
+        public Human(String name, String work, int age, String gender, double length, House house){
             this.name=name;
             this.work=work;
             this.age=age;
             this.gender=gender;
             this.length=length;
+            this.house=house;
         }
 
         public void printHumanDetails () {
@@ -50,6 +71,7 @@
             System.out.println("Age: " +age+ " years old");
             System.out.println("Gender: " +gender);
             System.out.println("Length: " +length+ " cm");
+            System.out.println("House: " +house);
         }
 
 
