2105791e4ceec6e08437b10d322405dcf4657cf1
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-09-24 12:27:18 +0200

    Dice done

--- codealongs/HelloWorld/Personlig/DiceGame/Die.java
+++ codealongs/HelloWorld/Personlig/DiceGame/Die.java
@@ -2,5 +2,25 @@ package DiceGame;
 import java.util.Random;
 
 public class Die {
+    private int currentValue;
+    private int sides;
+    private Random random = new Random();
+
+    public Die(int sides) {
+        this.sides=sides;
+
+    }
     
+    public int getSides(int sides) {
+        return sides;
+    }
+
+    public int getCurrentValue() {
+        return currentValue;
+    }
+
+    public void roll() {
+        currentValue = random.nextInt(sides) + 1;
+    }
 }
+ 
