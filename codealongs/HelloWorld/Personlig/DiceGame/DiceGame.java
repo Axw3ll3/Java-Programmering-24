bd8b9f8ff7390e13fc7bd2a79059d9639dde8f40
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-10-03 20:19:32 +0200

    Mer uppdateringar

--- codealongs/HelloWorld/Personlig/DiceGame/DiceGame.java
+++ codealongs/HelloWorld/Personlig/DiceGame/DiceGame.java
@@ -1,11 +1,9 @@
 package DiceGame;
-
+import java.util.Scanner;
 
 public class DiceGame {
-    
-
-
     public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Die die = new Die(6);
     }
 }
