789647ccc85ea83a43eca72c38ae5e933e51b780
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-10-07 11:44:33 +0200

    removal and start of new die class

---
+++ codealongs/HelloWorld/Personlig/Lab1_DiceGame/Die.java
@@ -0,0 +1,28 @@
+package Lab1_DiceGame;
+import java.util.Random;
+
+public class Die {
+    private static Random random = new Random();
+
+    private int currentValue;
+    private int sides;
+
+    public Die(int sides) {
+        this.sides=sides;
+        this.currentValue=currentValue;
+    }
+
+    public int getSides(){
+        return sides;
+    }
+
+    public int getCurrentValue(){
+        return currentValue;
+    }
+
+    public void roll(){
+        currentValue=random.nextInt(sides);
+    }
+
+}
+
