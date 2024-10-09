548363e6e66f4e07a9f2eae9d36c9399ea8421cc
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-10-09 11:51:53 +0200

    Removal of code, moved to "Game Engine" class

--- codealongs/HelloWorld/Personlig/Lab1_DiceGame/DiceGame.java
+++ codealongs/HelloWorld/Personlig/Lab1_DiceGame/DiceGame.java
@@ -3,39 +3,6 @@ import java.util.Scanner;
 
 public class DiceGame {
         public static void main (String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            
-            System.out.println("Select amount of rounds: ");
-            int rounds = scanner.nextInt();
-    
-            System.out.println("Please, enter your name: ");
-            String playerName = scanner.next();
-            
-            System.out.println("How many sides do you want the dice to have?");
-            int sides = scanner.nextInt();
-    
-            Player player = new Player(playerName);
-            player.addDie(sides);
-    
-            for (int i = 1; i<=rounds; i++) {
-                System.out.println("\nOmgång " + i);
-    
-                System.out.println("Gissa ett värde mellan 1 och " +sides+ " : ");
-                int guess = scanner.nextInt();
-                
-                player.rollDice();
-                int dieValue = player.getDieValue();
-                System.out.println("Tärningen visade: " +dieValue);
-    
-                if (guess == dieValue) {
-                    player.increaseScore();
-                    System.out.println("Grattis! Du fick rätt och 1 poäng");
-                }
-                else {
-                    System.out.println("Tyvärr, du gissade fel.");
-                }
-            }
-            System.out.println("\nSpelet är över! " + player.getName() + " du fick totalt: " +player.getScore() + " poäng.");
-    
-        }
+            GameEngine engine = new GameEngine();
+            engine.start();
     }
