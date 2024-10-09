83db6d87ecfb607152998d400c25720963b31a19
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-10-09 13:21:24 +0200

    Changes in class

--- codealongs/HelloWorld/Personlig/Lab1_DiceGame/DiceGame.java
+++ codealongs/HelloWorld/Personlig/Lab1_DiceGame/DiceGame.java
@@ -4,5 +4,6 @@ import java.util.Scanner;
 public class DiceGame {
         public static void main (String[] args) {
             GameEngine engine = new GameEngine();
-            engine.start();
+            engine.Launch();
     }
+}
