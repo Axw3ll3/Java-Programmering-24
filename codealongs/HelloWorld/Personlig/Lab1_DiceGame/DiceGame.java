6ac735239a493c524766d06d9b57fcb61d37a0cb
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-10-13 14:31:06 +0200

    DiceGame Multiplayer class done

--- codealongs/HelloWorld/Personlig/Lab1_DiceGame/DiceGame.java
+++ codealongs/HelloWorld/Personlig/Lab1_DiceGame/DiceGame.java
@@ -2,7 +2,6 @@ package Lab1_DiceGame;
 
 public class DiceGame {
         public static void main (String[] args) {
-            GameEngine engine = new GameEngine();
-            engine.Launch();
+            MultiplayerDiceGame.playGame();
     }
 }
