d8f7456d9d06d44dc15d79fa03dfacd954629e87
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-10-24 22:03:25 +0200

    Creation of Fire class

--- codealongs/HelloWorld/Personlig/AventureGame.java/Fire.java
+++ codealongs/HelloWorld/Personlig/AventureGame.java/Fire.java
@@ -1,5 +1,30 @@
 package AventureGame.java;
 
 public class Fire {
-    
+    private boolean isLit;
+    private String choice;
+
+    public Fire() {
+        this.isLit = false;
+    }
+
+    public void askToLight() {
+        //Metod för att hämta valmeny
+
+    }
+
+    public void ifYes() {
+        isLit = true;
+        System.out.println("You've decided to start the fire");
+        intelligence += 1;
+        System.out.println("You've also gotten a skillpoint for intelligence.");
+        System.out.println("Unfortunally, you attracted spiders");
+        //Metod för att kalla på spindlar
+    }
+    public void ifNo() {
+        isLit = false;
+        System.out.println("Due to you not putting up the fire, you've gotten cold and lost a healthpoint.");
+        health -=1;
+
+    }
 }
