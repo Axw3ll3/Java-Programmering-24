29376d010a26ef2b4834249483d86d182d81634a
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-10-26 01:45:44 +0200

    Food done + check in GameEngine if food works

--- codealongs/HelloWorld/Personlig/Adventure_Game/GameEngine.java
+++ codealongs/HelloWorld/Personlig/Adventure_Game/GameEngine.java
@@ -3,13 +3,22 @@ package Adventure_Game;
 public class GameEngine {
     public static void main(String[] args) {
         Slowprint slowprint = new Slowprint();
+        /*
         FirePlace fire = new FirePlace();
         fire.arrivalAtFirePlace();
         System.out.println("");
         fire.ifLit();
         System.out.println("");
         fire.ifNotLit();
-
+        */
+        
+        Food food = new Food();
+        System.out.println("");
+        food.findingTheFood();
+        System.out.println("");
+        food.eatingTheFood();
+        System.out.println("");
+        food.notEatingTheFood();
     }
     
 }
