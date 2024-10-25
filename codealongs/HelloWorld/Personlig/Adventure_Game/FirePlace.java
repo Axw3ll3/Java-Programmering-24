2817d9bdef3ef93a6c18ace92e60cd957a9d6340
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-10-25 20:05:47 +0200

    Rename of Fire class to FirePlace, and test round in GameEngine class

---
+++ codealongs/HelloWorld/Personlig/Adventure_Game/FirePlace.java
@@ -0,0 +1,44 @@
+package Adventure_Game;
+
+public class FirePlace {
+    //Glöm inte att ta bort health, intellegence och slowprint sen.
+    private boolean isLit;
+    private String choice;
+    Slowprint slowprint = new Slowprint();
+    int health = 3;
+    int intelligence = 0;
+
+    public FirePlace() {
+        this.isLit = false;
+    }
+
+    public void arrivalAtFirePlace(){
+        slowprint.slowPrintln("The traveler emerged from the dense forest as dusk crept in, weary and cold. Through the trees, you spot a stone-ringed "
+         + "fireplace in a small clearing, untouched and waiting in the twilight. You approach, noting the silence around it. "
+         + "No firewood was stacked, no sign of another soul - only the cold stones and the faint scent of old ashes. Kneeling down "
+         + "you brushed your right hand over the stones, feeling a chill that ran deeper than the night air. You hesitate, then decide to gather some kindling nearby, "
+         + "preparing to bring warmth back to this quiet, abandoned place. You think twice, do I want to start a fire or not? "
+         + "What if I attract unwanted creatures towards me? Do you still want to lit the fireplace?", 20);
+    }
+
+    public void askToLit() {
+        //Metod för att hämta valmeny
+
+    }
+
+    public void ifLit() {
+        isLit = true;
+        slowprint.slowPrintln("You've decided to lit the fire.", 50);
+        intelligence += 1;
+        slowprint.slowPrintln("You've also gotten a skillpoint for intelligence! " + "Unfortunally, you attracted spiders towards your position." ,50);
+        //Metod för att kalla på spindlar
+    }
+    public void ifNotLit() {
+        isLit = false;
+        slowprint.slowPrintln("You've decided to not lit the fire.", 50);
+        health -=1;
+        slowprint.slowPrintln("Due to you not putting up the fire, you've gotten cold and lost a healthpoint. " + "-1 Healthpoint, you still got "+health+ " left");
+        slowprint.slowPrintln("You've moved on without a scratch though, which let you move on to the next part of the story");
+        //Metod för att ta sig vidare i historien
+    }
+}
--- codealongs/HelloWorld/Personlig/Adventure_Game/GameEngine.java
+++ codealongs/HelloWorld/Personlig/Adventure_Game/GameEngine.java
@@ -3,7 +3,11 @@ package Adventure_Game;
 public class GameEngine {
     public static void main(String[] args) {
         Slowprint slowprint = new Slowprint();
-        slowprint.slowPrintln("Hej, jag heter Axel och är 21 år gammal ", 50);
+        FirePlace fire = new FirePlace();
+        fire.arrivalAtFirePlace();
+        fire.ifLit();
+        fire.ifNotLit();
+
     }
     
 }
