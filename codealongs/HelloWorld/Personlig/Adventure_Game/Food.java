c93ff5112484a915b52fd6f1b714ce927716fddc
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-10-25 20:27:00 +0200

    Start of food class and methods

--- codealongs/HelloWorld/Personlig/Adventure_Game/Food.java
+++ codealongs/HelloWorld/Personlig/Adventure_Game/Food.java
@@ -1,5 +1,27 @@
 package Adventure_Game;
+import java.util.Random;
 
 public class Food {
-    
+    private boolean isPoisinous;
+    Random r = new Random();
+    int random = r.nextInt(2);
+    Slowprint slowprint = new Slowprint();
+
+
+    public Food() {
+        this.isPoisinous=isPoisinous;
+    }
+
+    public void findingTheFood() {
+        slowprint.slowPrintln("You find random food");
+    }
+
+    public void eatingTheFood() {
+
+    }
+
+    public void notEatingTheFood() {
+
+    }
+
 }
