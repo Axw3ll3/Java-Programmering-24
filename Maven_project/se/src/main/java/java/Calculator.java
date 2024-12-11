7ffd18e93d9289e8610ad5d8641d1e43dc2509dc
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-12-11 10:38:45 +0100

    Oklart vad fan som händer

--- Maven_project/se/src/main/java/java/Calculator.java
+++ Maven_project/se/src/main/java/java/Calculator.java
@@ -1,7 +1,22 @@
 package java;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
 
 public class Calculator {
-    public int add()
+    public int add(int num1, int num2) {
+        int result = num1 + num2;
+        return result;
+    }
+    public int substract(int num1, int num2) {
+        int result = num1 - num2;
+        return result;
+    }
+
+    public int multiply(int num1, int num2) {
+        int result = num1 * num2;
+        return result;
+    }
+
+    public double divide(int num1, int num2) {
+        int result = num1/num2;
+        return result;
+    }
 }
--- Maven_project/se/src/test/java/Calculatortest.java
+++ Maven_project/se/src/test/java/Calculatortest.java
@@ -1,3 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class Calculatortest {
-    
+    @Test
+    public void test1Plus1(){
+        assertEquals(1, 1);
+    }
 }
