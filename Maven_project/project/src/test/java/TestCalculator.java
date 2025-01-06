82bd8cfda986a3c4bffe088a07e3eb9ca275f9c9
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2025-01-06 12:09:17 +0100

    Changes

--- Maven_project/project/src/test/java/TestCalculator.java
+++ Maven_project/project/src/test/java/TestCalculator.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 public class TestCalculator {
     @Test
     public void test1Plus1(){
-        assertEquals(1, 1);
+        assertEquals(2, 2);
         }
-    
+
 }
