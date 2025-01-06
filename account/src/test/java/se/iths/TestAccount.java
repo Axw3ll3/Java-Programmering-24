17af921489006956b116efb7fed9dc5035b11c33
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2025-01-06 14:17:18 +0100

    Facts

--- account/src/test/java/se/iths/TestAccount.java
+++ account/src/test/java/se/iths/TestAccount.java
@@ -2,12 +2,13 @@ package se.iths;
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class TestAccount {
     @Test
     public void testCreateNewAccount() {
         Account account = new Account(100);
-
         assertEquals(100, account.balance);
     }
 }
